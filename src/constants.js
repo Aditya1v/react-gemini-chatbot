1a803762469c683b7ecd99f272bb87a5af40525e
Aditya <adivermapro@gmail.com>
2026-03-19 22:37:27 +0530

    update

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,4 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyDmMBLUNKXuh9MWGVyNG_BcnzC3DAM-A54";
+export const URL = import.meta.env.VITE_API_KEY;
 
-
-
-
-
-// export const URL = curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" \
-//   -H 'Content-Type: application/json' \
-//   -H 'X-goog-api-key: AIzaSyCbCP2NWOaYbshYa5KXqMCQR-zLAr_jSr8' \
-//   -X POST \
-//   -d '{
-//     "contents": [
-//       {
-//         "parts": [
-//           {
-//             "text": "Explain how AI works in a few words"
-//           }
-//         ]
-//       }
-//     ]
-//   }'
 
 
